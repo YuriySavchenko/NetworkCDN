028c140001b89c7bb761d14c260531c2c3c28338
YuriySavchenko <yurec.proger@gmail.com>
2019-06-20 22:55:18 +0300

    added functions and methods for searching optimal path

--- headers/CDN/cdn.h
+++ headers/CDN/cdn.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <QVector>
+#include <QQueue>
 
 #include "../../headers/Graph/edge.h"
 #include "../../headers/CDN/matrix.h"
@@ -23,14 +24,31 @@ public:
 private:
     Matrix matrix;                  //!< A \e variable \b matrix for saving \b hosts
     QVector<Edge *> edges;          //!< A \e variable for saving \a vector of \b edges
+    QVector<QVector<int>> paths;    //!< A \e variable for saving all \a paths of \b graph between \a source node and \a dest node
 
 public:
-    QVector<Edge *>& getEdges();    //!< A \e function for getting \b edges
-    Matrix& getMatrix();            //!< A \e function for getting \b matrix
+    QVector<Edge *>& getEdges();            //!< A \e function for getting \b edges
+    Matrix& getMatrix();                    //!< A \e function for getting \b matrix
 
 public:
     void setEdges(const QVector<Edge *> &);     //!< A \e method for saving \a vector of \b edges
     void setMatrix(const Matrix &);             //!< A \e method for saving \b matrix
+
+public:
+    void addPath(const QVector<int> &path);                     //!< A \e method for saving one \a path of \b graph
+    QVector<int> findPaths(const int &src, const int &dst);     //!< A \e method for searching neighbors \a nodes in \b graph
+
+public:
+    //!
+    //! \brief findPathsUtil
+    //! \param src
+    //! \param dst
+    //! \param map
+    //! \param visited
+    //! \param path
+    //! \param path_index
+    //!
+    void findPathsUtil(const int &src, const int &dst, QMap<int, QVector<int>> map, QVector<bool> &visited, QVector<int> &path, int &path_index);
 };
 
 #endif // CDN_H
